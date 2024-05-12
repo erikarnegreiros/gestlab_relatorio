ea3748b13cfa4c3b97d7b8dc1eade6b45e2ced4b
Erika Negreiros <negreiros.rocha@gmail.com>
2024-05-11 22:28:53 -0300

    Adicionar arquivos de projeto.

---
+++ gestlab_relatorio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gestlab_relatorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
